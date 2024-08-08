9e4a0c3abfd94b4a44808e60fb5bf339ab36349d
Ibrahim <ibrahimramadan441999@gmail.com>
2024-08-08 22:22:43 +0300

    fixing auth server

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-// server.js
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
-const server = jsonServer.create();
-const router = jsonServer.router("./db.json");
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-server.use(auth);
-server.use(router);
+const PORT = 9000;
+app.db = router.db;
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log("JSON Server with Auth is running");
+app.use(middlewares);
+app.use(auth);
+app.use(router);
+app.listen(PORT, () => {
+  console.log("Server is Running");
+  console.log("http://localhost:" + PORT);
 });
+module.exports = app;
